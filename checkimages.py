6c3c31ee76302a20f691e606291ec4427b70a57d
rubinsztajn <rubinsztajn@gmail.com>
2014-02-18 15:40:04 -0500

    Updated checkimages to most recent version

--- checkimages.py
+++ checkimages.py
@@ -1,45 +1,47 @@
 #!c:/Python/python.exe -u
-# $ checkimages.py /path/to/files
+# $ checkimages.py resolution /path/to/files
 
 import EXIF
 import glob
 import sys
 import Image
+import TiffImagePlugin
 from shutil import copy2
+
+dpi = sys.argv[1]
 
 filenum = 0
 bitdepthnum = 0
 lzw = 0
-for file in glob.iglob(sys.argv[1]):
+
+for filename in glob.iglob(sys.argv[2]):
     filenum += 1
     try: 
-        tags = EXIF.process_file(open(file))
+        tags = EXIF.process_file(open(filename))
         res = tags['Image YResolution'].printable
         compr = tags['Image Compression'].printable
         date = tags['Image DateTime'].printable
 
-        if res != '300':
-            print "File: %s  Res: %s dpi  Date: %s" % (file, res, date)    
+        if res != dpi:
+            print "File: %s  Res: %s dpi  Date: %s" % (filename, res, date)    
 
         if compr != 'LZW':
-            copy2(file, 'u:/image_sandbox/compress')
+            copy2(filename, 'c:/image_sandbox/compress')
             lzw += 1
 
     except:
-        print "Can't read EXIF tags: %s" % (file)
+        print "Can't read EXIF tags: %s" % (filename)
 
     try:
-        i = Image.open(file)
+        i = Image.open(filename)
     except IOError:
-        copy2(file, 'u:/image_sandbox/bitdepthissues/')
+        copy2(filename, 'c:/image_sandbox/bitdepthissues/')
         bitdepthnum += 1
 
     
-itemnum = file[14:17]
 print 
 print "------------------"
 print "%d total files" % (filenum)
-print "%s total items" % (itemnum)
 if bitdepthnum:
     print "%d total bitdepth issues" % (bitdepthnum)
 if lzw:
